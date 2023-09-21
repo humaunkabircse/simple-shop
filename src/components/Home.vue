a36fb45a552ccebcece398441931e7e327f48cad
kabircse2018 <kabircse2018@gmail.com>
2023-09-21 21:09:46 +0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main class="bg-gray-100 min-h-screen h-auto z-10 py-8">
+    <section class="container mx-auto">
+      <div>
+        <h3 class="text-2xl font-inter font-semibold py-2 border-b-2">Home</h3>
+      </div>
+      
+    </section>
+  </main>
+</template>
